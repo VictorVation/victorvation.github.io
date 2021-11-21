2f4668d2553f4c989a0c845556035125229c7d12
Victor Szeto <vis@vis-air.local>
2021-11-20 16:43:13 -0800

    update

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Victor Szeto',
+    site_url: 'https://victorszeto.com',
+    feed_url: 'https://victorszeto.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
