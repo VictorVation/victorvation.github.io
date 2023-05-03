c666504bb947e0924d8ee160362f4421824409cd
Victor Szeto <v.victorszeto@gmail.com>
2023-05-03 14:45:52 -0400

    enable static image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-blog',
-  themeConfig: './theme.config.js'
+  themeConfig: './theme.config.js',
+  staticImage: true
 })
 
 module.exports = withNextra()
